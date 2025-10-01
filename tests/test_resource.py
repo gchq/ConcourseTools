8abe0a591132e6919c36672788ec10b566445267
gchqdev227 <62302861+gchqdev227@users.noreply.github.com>
2025-10-01 11:32:17 +0100

    Replaced base image with newer version

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -15,6 +15,8 @@ from concoursetools.testing import (ConversionTestResourceWrapper, DockerConvers
                                     run_command)
 from tests.resource import ConcourseMockResource, ConcourseMockVersion, TestResource, TestVersion
 
+CONCOURSE_MOCK_RESOURCE_IMAGE = "concourse/mock-resource:0.14.1-20250809"
+
 
 class SimpleWrapperTests(TestCase):
 
@@ -742,7 +744,6 @@ def _build_test_resource_docker_image() -> str:
 
 
 class ExternalDockerWrapperTests(TestCase):
-    image = "concourse/mock-resource:0.13.0"
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -756,7 +757,7 @@ class ExternalDockerWrapperTests(TestCase):
             "log": "Debug message",
             "metadata": [{"name": "key", "value": "value"}],
         }
-        self.wrapper = DockerTestResourceWrapper(config, self.image)
+        self.wrapper = DockerTestResourceWrapper(config, CONCOURSE_MOCK_RESOURCE_IMAGE)
 
     def test_check_step_with_version_no_debugging(self) -> None:
         version_config = {"version": "1", "privileged": "true"}
@@ -929,7 +930,6 @@ class ExternalDockerWrapperTests(TestCase):
 
 
 class ExternalDockerConversionWrapperTests(TestCase):
-    image = "concourse/mock-resource:0.13.0"
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -943,7 +943,7 @@ class ExternalDockerConversionWrapperTests(TestCase):
             "log": "Debug message",
             "metadata": [{"name": "key", "value": "value"}],
         }
-        self.wrapper = DockerConversionTestResourceWrapper(ConcourseMockResource, config, self.image)
+        self.wrapper = DockerConversionTestResourceWrapper(ConcourseMockResource, config, CONCOURSE_MOCK_RESOURCE_IMAGE)
 
     def test_check_step_with_version_no_debugging(self) -> None:
         version = ConcourseMockVersion(version=1, privileged=True)
